7fc991030ac2555253e18428d5fac4f7aaef838f
EasyItsMe <adroitahmadzaki@gmail.com>
2026-09-20 20:54:31 +0700

    feat: implement Option 4 diagonal scrapbook corner masking tape on polaroid cards

--- js/app.js
+++ js/app.js
@@ -510,7 +510,8 @@ class BirthdayApp {
 
     container.innerHTML = RomanticConfig.memories.map((m, idx) => `
       <div class="polaroid-card" onclick="window.birthdayApp.openLightbox(${idx})">
-        <div class="washi-tape"></div>
+        <div class="corner-tape corner-tape-tl"></div>
+        <div class="corner-tape corner-tape-tr"></div>
         <div class="polaroid-img-box">
           <img src="${m.img}" alt="${m.title}" loading="lazy" />
         </div>
